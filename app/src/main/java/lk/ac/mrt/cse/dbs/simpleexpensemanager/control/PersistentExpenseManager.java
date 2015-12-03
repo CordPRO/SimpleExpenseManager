844cd3e0ef07b7300db6002b501c61e9627c10f0
Madhawa <mvidanapathirana@live.co.uk>
2015-12-03 17:04:45 +0530

    Initial Commit

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -0,0 +1,14 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+
+/**
+ * Created by Madhawa on 03/12/2015.
+ */
+public class PersistentExpenseManager extends ExpenseManager {
+    @Override
+    public void setup() throws ExpenseManagerException {
+        
+    }
+}
